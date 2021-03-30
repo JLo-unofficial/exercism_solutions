a62b43703f9a99b83413df6b494c919fa100069b
OtherwiseJLo <jonathanglopez@gmail.com>
2021-03-30 07:37:49 -0400

    Adding comments

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,10 +1,16 @@
+/*
+Package hamming is a simple Hamming distance calculation package
+*/
 package hamming
 
 import "errors"
 
+// Distance accepts two DNA strings of equal length and returns their
+// Hamming distance
+// If the strings are not of equal length, Distance raises an error
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
-		return 0, errors.New("Strand lengths are not equal")
+		return 0, errors.New("Lengths of strings do not match")
 	}
 	distance_count := 0
 	for i := 0; i < len(a); i++ {
