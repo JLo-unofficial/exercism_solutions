e5a4b5498eec4ac55bbd057ebb9d8b576352e105
OtherwiseJLo <jonathanglopez@gmail.com>
2021-04-01 08:52:13 -0400

    Submitted golang track scrabble score exercise

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,45 @@
+/*
+Package scrabble implements a simple scrabble scoring function
+*/
+package scrabble
+
+import "strings"
+
+var letterScores = map[string]int{
+	"A": 1,
+	"B": 3,
+	"C": 3,
+	"D": 2,
+	"E": 1,
+	"F": 4,
+	"G": 2,
+	"H": 4,
+	"I": 1,
+	"J": 8,
+	"K": 5,
+	"L": 1,
+	"M": 3,
+	"N": 1,
+	"O": 1,
+	"P": 3,
+	"Q": 10,
+	"R": 1,
+	"S": 1,
+	"T": 1,
+	"U": 1,
+	"V": 4,
+	"W": 4,
+	"X": 8,
+	"Y": 4,
+	"Z": 10,
+}
+
+// Score accepts a word and returns the expected scrabble score based
+// on letterScores map
+func Score(word string) int {
+	score := 0
+	for _, letter := range strings.Split(word, "") {
+		score += letterScores[strings.ToUpper(letter)]
+	}
+	return score
+}
