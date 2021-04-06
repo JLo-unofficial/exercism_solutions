512e802d17934e7dc8931af40d4cf84877baf90e
OtherwiseJLo <jonathanglopez@gmail.com>
2021-04-06 06:57:48 -0400

    Passed all isogram tests

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,1 +1,26 @@
+/*
+Package isogram is a simple package to determine if a word is an isogram
+*/
 package isogram
+
+import (
+	"regexp"
+	"unicode"
+)
+
+// Handy pattern to match all non-alphanumeric characters
+var nonAlphaNumeric, err = regexp.Compile(`\W`)
+
+// IsIsogram accepts any string and returns true if the string is an isogram
+// and false otherwise
+func IsIsogram(word string) bool {
+	visited := map[rune]int{}
+	for _, letter := range nonAlphaNumeric.ReplaceAllString(word, "") {
+		_, seenBefore := visited[unicode.ToUpper(letter)]
+		if seenBefore {
+			return false
+		}
+		visited[unicode.ToUpper(letter)] = 1
+	}
+	return true
+}
