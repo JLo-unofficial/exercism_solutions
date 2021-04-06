e2347ac58e1c979c71a6ec4df3e2e52f339c7397
OtherwiseJLo <jonathanglopez@gmail.com>
2021-04-05 22:17:38 -0400

    Adding Lua summation of squares exercise

---
+++ lua/difference-of-squares/difference-of-squares.lua
@@ -0,0 +1,22 @@
+-- Returns the square of the summation of integers from 1 to n
+local function square_of_sum(n)
+  local sum = ((n+1)*n)/2
+  return sum*sum
+end
+
+-- Returns the summation of squares from 1^2 to n^2
+local function sum_of_squares(n)
+  return (n*(n+1)*(2*n+1))/6
+end
+
+-- Returns the difference between the square of sums and the sum of squares
+local function difference_of_squares(n)
+  return square_of_sum(n) - sum_of_squares(n)
+
+end
+
+return {
+  square_of_sum = square_of_sum,
+  sum_of_squares = sum_of_squares,
+  difference_of_squares = difference_of_squares
+}
