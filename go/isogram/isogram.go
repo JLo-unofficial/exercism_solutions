38d424d44bbdb176454f29a5f6bbb8bfbdd79ba7
jon.lopez <jonathan.lopez@gridunity.com>
2021-06-02 11:24:36 -0400

    Removed unused function

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -5,31 +5,18 @@ package isogram
 
 import "unicode"
 
-// LetterToIndex converts an alphabetic rune to its index, ignoring case
-func LetterToIndex(letter rune) rune {
-	// Lowercase letters are more common and can return an index earlier
-	if letter >= 'a' && letter <= 'z' {
-		return letter - 'a'
-	}
-	if letter >= 'A' && letter <= 'Z' {
-		return letter - 'A'
-	}
-
-	// If the rune is non-alphabetic, return empty rune
-	return ' '
-}
-
 // IsIsogram accepts any string and returns true if the string is an isogram
 // and false otherwise without using regular expressions
 func IsIsogram(word string) bool {
 	visited := map[rune]bool{}
 	for _, letter := range word {
 		if unicode.IsLetter(letter) {
+			lowercase := unicode.ToLower(letter)
 			// Idiomatic way to check for value in map
-			if _, seenBefore := visited[unicode.ToLower(letter)]; seenBefore {
+			if _, seenBefore := visited[lowercase]; seenBefore {
 				return false
 			}
-			visited[unicode.ToLower(letter)] = true
+			visited[lowercase] = true
 		}
 	}
 	return true
