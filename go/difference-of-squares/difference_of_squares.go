dada4a73d18a2a082e21b0c071b833b60e6198f1
jon.lopez <jonathan.lopez@gridunity.com>
2021-06-04 09:11:48 -0400

    Updated comments

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -2,23 +2,25 @@
 // summation
 package diffsquares
 
+// The commented code below produced a worse benchmark than simply multiplying
+// the local summation value by itself
 // import "math"
 /* func SquareOfSum(n int) int {
 	summation := float64(n * (n + 1) / 2)
 	return int(math.Pow(summation, 2))
 } */
 
-// SumOfSquares accepts an int n and returns the summation of all squares
-// from 1 to n using the succinct formula
-func SumOfSquares(n int) int {
-	return (n * (n + 1) * (2*n + 1)) / 6
-}
-
 // SquareOfSums accepts an int n and returns the squared value of the summation
 // from 1 to n
 func SquareOfSum(n int) int {
 	summation := (n * (n + 1) / 2)
 	return summation * summation
+}
+
+// SumOfSquares accepts an int n and returns the summation of all squares
+// from 1 to n using the succinct formula
+func SumOfSquares(n int) int {
+	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
 // Difference accepts an int n and returns the difference between the
