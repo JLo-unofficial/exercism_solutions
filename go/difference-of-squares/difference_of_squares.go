018dd654839718676c3ed8e874e94898a528a366
jon.lopez <jonathan.lopez@gridunity.com>
2021-06-04 09:10:15 -0400

    Removed need for math lib

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,16 +1,28 @@
+// Package diffsquares implements a simple library to calculate sum of squares and the square of
+// summation
 package diffsquares
 
-import "math"
+// import "math"
+/* func SquareOfSum(n int) int {
+	summation := float64(n * (n + 1) / 2)
+	return int(math.Pow(summation, 2))
+} */
 
+// SumOfSquares accepts an int n and returns the summation of all squares
+// from 1 to n using the succinct formula
 func SumOfSquares(n int) int {
 	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
+// SquareOfSums accepts an int n and returns the squared value of the summation
+// from 1 to n
 func SquareOfSum(n int) int {
-	summation := float64(n * (n + 1) / 2)
-	return int(math.Pow(summation, 2))
+	summation := (n * (n + 1) / 2)
+	return summation * summation
 }
 
+// Difference accepts an int n and returns the difference between the
+// SquareOfSums and the SumOfSquares
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
