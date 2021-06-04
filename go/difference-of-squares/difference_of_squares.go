31bd6d1e675bd88032eb3b6ce4490877b618ff9f
jon.lopez <jonathan.lopez@gridunity.com>
2021-06-04 08:56:19 -0400

    Passed all tests

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,1 +1,16 @@
 package diffsquares
+
+import "math"
+
+func SumOfSquares(n int) int {
+	return (n * (n + 1) * (2*n + 1)) / 6
+}
+
+func SquareOfSum(n int) int {
+	summation := float64(n * (n + 1) / 2)
+	return int(math.Pow(summation, 2))
+}
+
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
