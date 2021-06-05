814be501fd707bb7318c53d58da40dc989a32068
OtherwiseJLo <jonathanglopez@gmail.com>
2021-06-05 13:15:27 -0400

    Refactored solution

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,35 +1,21 @@
 #include "armstrong_numbers.h"
 #include <math.h>
 
-int count_digits(int candidate) {
+// local static helper function
+static int count_digits(int candidate) {
   int count = 0;
-  int mod;
-  while (candidate > 0) {
-    mod = candidate % 10;
-    candidate -= mod;
-    candidate /= 10;
+  // more succinct loop to increment count
+  for (; candidate > 0; candidate /= 10) {
     count++;
   }
   return count;
 }
 
-int calculate_armstrong_number(int digit_count, int candidate) {
-  int accumulator = 0;
-  int mod;
-  while (candidate > 0) {
-    mod = candidate % 10;
-    accumulator += pow(mod, digit_count);
-    candidate -= mod;
-    candidate /= 10;
-  }
-  return accumulator;
-}
-
 int is_armstrong_number(int candidate) {
-  if (candidate == 0) {
-    return 1;
-  }
   int digit_count = count_digits(candidate);
-  int armstrong_number = calculate_armstrong_number(digit_count, candidate);
+  int armstrong_number = 0;
+  for (int i = candidate; i > 0; i /= 10) {
+    armstrong_number += pow(i % 10, digit_count);
+  }
   return armstrong_number == candidate;
 }
