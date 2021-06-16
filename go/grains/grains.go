dc744b17ed084fc0a1c815f6115e67382fb121b2
jon.lopez <jonathan.lopez@gridunity.com>
2021-06-16 09:40:12 -0400

    Simplified total

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,19 +1,19 @@
+// Package grains implements a simple doubling game played on a chessboard
 package grains
 
 import "errors"
 
+// Square accepts a valid chessboard square index and returns the number of grains
+// found on that square
 func Square(squareIdx int) (uint64, error) {
 	if squareIdx < 1 || squareIdx > 64 {
 		return 0, errors.New("Bitshifts must be in range [1,64] for a uint64")
 	}
+
 	return uint64(1 << (squareIdx - 1)), nil
 }
 
+// Total returns the number of grains on the entire chessboard
 func Total() uint64 {
-	var total uint64 = 0
-	for i := 1; i <= 64; i++ {
-		val, _ := Square(i)
-		total += val
-	}
-	return total
+	return ^uint64(0)
 }
