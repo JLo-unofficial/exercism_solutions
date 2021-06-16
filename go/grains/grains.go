6b7db051404172fc26f8a7987e41d5a88df3766e
jon.lopez <jonathan.lopez@gridunity.com>
2021-06-16 09:34:40 -0400

    Passed all tests

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,1 +1,19 @@
 package grains
+
+import "errors"
+
+func Square(squareIdx int) (uint64, error) {
+	if squareIdx < 1 || squareIdx > 64 {
+		return 0, errors.New("Bitshifts must be in range [1,64] for a uint64")
+	}
+	return uint64(1 << (squareIdx - 1)), nil
+}
+
+func Total() uint64 {
+	var total uint64 = 0
+	for i := 1; i <= 64; i++ {
+		val, _ := Square(i)
+		total += val
+	}
+	return total
+}
