37cdd771b54a43b30ecca1d48b05ae6302b87e72
OtherwiseJLo <jonathanglopez@gmail.com>
2021-06-16 08:09:20 -0400

    Passed all tests

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,20 +1,23 @@
 // Package luhn is a simple implementation of the Luhn algorithm
 package luhn
 
-import "unicode"
+import (
+	"strings"
+	"unicode"
+)
 
 // luhnMap provides a quick lookup for every other digit
-var luhnMap = map[rune]int{
-	'0': 0,
-	'1': 2,
-	'2': 4,
-	'3': 6,
-	'4': 8,
-	'5': 1,
-	'6': 3,
-	'7': 5,
-	'8': 7,
-	'9': 9,
+var luhnMap = map[rune]rune{
+	'0': '0',
+	'1': '2',
+	'2': '4',
+	'3': '6',
+	'4': '8',
+	'5': '1',
+	'6': '3',
+	'7': '5',
+	'8': '7',
+	'9': '9',
 }
 
 // Valid accepts a candidate string and determines whether the string is valid
@@ -23,27 +26,21 @@ func Valid(candidate string) bool {
 	doubleValue := true
 	digitCount := 0
 	sum := 0
-	// Iterate backwards over candidate string
+	// Iterate backwards over candidate string stripped of spaces
+	candidate = strings.ReplaceAll(candidate, " ", "")
 	for i := len(candidate) - 1; i >= 0; i-- {
 		char := rune(candidate[i])
-
-		// Candidate is invalid if it contains a letter or punctuation
-		if unicode.IsLetter(char) || unicode.IsPunct(char) {
+		// candidate is invalid if it contains a non-digit rune
+		if !unicode.IsDigit(char) {
 			return false
 		}
 
-		// Ignore char if not a digit
-		if !unicode.IsDigit(char) {
-			continue
+		digitCount++
+		if doubleValue = !doubleValue; doubleValue { // cycles between true and false
+			char = luhnMap[char]
 		}
 
-		digitCount ++
-		doubleValue = !doubleValue // cycles between true and false
-		if doubleValue {
-			sum += luhnMap[char]
-		} else {
-			sum += int(char - '0')
-		}
+		sum += int(char - '0')
 	}
 
 	return digitCount > 1 && sum%10 == 0
