9b04af507862bb563518c66a9471b55cd941eb29
jon.lopez <jonathan.lopez@gridunity.com>
2021-06-17 14:23:33 -0400

    Add comments

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -4,4 +4,6 @@ const rnaMapping = {
     'G': 'C',
     'T': 'A',
 }
+
+// convert string to array of strings in order to apply map
 export const toRna = (dna) => dna.split('').map(letter => rnaMapping[letter]).join('');
