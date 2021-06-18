d341f37c64fd093a088759295b5864244d744d77
jon.lopez <jonathan.lopez@gridunity.com>
2021-06-18 09:23:13 -0400

    Refactored luhn code

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -6,38 +6,40 @@ import (
 	"unicode"
 )
 
-// Double accepts an integer and applies the Luhn transformational
-// step
-func Double(digit int) int {
-	digit = digit * 2
-	if digit > 9 {
-		digit -= 9
-	}
-	return digit
-}
-
 // Valid accepts a candidate string and determines whether the string is valid
 func Valid(candidate string) bool {
 
 	candidate = strings.ReplaceAll(candidate, " ", "")
-	doubleValue := len(candidate)%2 == 1
+
+	if len(candidate) <= 1 {
+		return false
+	}
+
+	doubleValue := len(candidate)%2 == 0
 	sum := 0
+
 	// Iterate backwards over candidate string stripped of spaces
 	for _, char := range candidate {
 		// candidate is invalid if it contains a non-digit rune
 		if !unicode.IsDigit(char) {
 			return false
 		}
+
 		digit := int(char - '0')
 
-		// cycles between true and false
-		doubleValue = !doubleValue
+		// apply luhn transformation to digit
 		if doubleValue {
-			digit = Double(digit)
+			digit *= 2
+			if digit > 9 {
+				digit -= 9
+			}
 		}
 
 		sum += digit
+
+		// cycles between true and false
+		doubleValue = !doubleValue
 	}
 
-	return len(candidate) > 1 && sum%10 == 0
+	return sum%10 == 0
 }
