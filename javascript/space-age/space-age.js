f71a2d9afcd4f833ad624defa37c55788fa77465
jon.lopez <jonathan.lopez@gridunity.com>
2021-06-18 10:11:33 -0400

    Passed all tests

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,8 +1,23 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const orbitalPeriod = {
+    'mercury': 0.2408467,
+    'venus': 0.61519726,
+    'earth': 1.0,
+    'mars': 1.8808158,
+    'jupiter': 11.862615,
+    'saturn': 29.447498,
+    'uranus': 84.016846,
+    'neptune': 164.79132,
 
-export const age = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+}
+
+const SECONDS_IN_MINUTE = 60.0;
+const MINUTES_IN_HOUR = 60.0;
+const HOURS_IN_DAY = 24.0;
+const DAYS_IN_YEAR = 365.25;
+const SECONDS_IN_YEAR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR * HOURS_IN_DAY * DAYS_IN_YEAR;
+
+export const age = (planet, ageInSeconds) => {
+    const preciseAge = (ageInSeconds / orbitalPeriod[planet]) / SECONDS_IN_YEAR;
+    // take advantage of Math.round and identity operation (x*100/100) to return result with two sigfigs
+    return Math.round(preciseAge * 100) / 100;
+}
