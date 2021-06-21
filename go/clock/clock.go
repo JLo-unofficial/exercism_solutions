26ca73fac6f469331c2a52f6b602f1e99cc737c5
OtherwiseJLo <jonathanglopez@gmail.com>
2021-06-21 17:41:20 -0400

    Adding code structure

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,1 +1,31 @@
 package clock
+
+type Clock struct {
+	hour   int
+	minute int
+}
+
+func New(hour, minute int) Clock {
+	return Clock{
+		hour:   hour,
+		minute: minute,
+	}
+}
+
+func (clock Clock) String() string {
+	return ""
+}
+
+func (clock Clock) Add(minutes int) Clock {
+	return Clock{
+		hour:   clock.hour,
+		minute: clock.minute,
+	}
+}
+
+func (clock Clock) Substract(minutes int) Clock {
+	return Clock{
+		hour:   clock.hour,
+		minute: clock.minute,
+	}
+}
