211b3c9ea62e5c11aa0ca572526a8a90dc953937
jon.lopez <jonathan.lopez@gridunity.com>
2021-06-22 11:43:49 -0400

    Got pointers and structs fixed

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,11 +1,31 @@
 package clock
 
+import "fmt"
+
 type Clock struct {
 	hour   int
 	minute int
 }
 
 func New(hour, minute int) Clock {
+	if minute >= 60 {
+		hour += minute / 60
+		minute = minute % 60
+	}
+	if minute < 0 {
+		hour += minute / 60
+		minute = -1 * (minute % 60)
+	}
+	if minute <= -60 {
+	}
+
+	if hour >= 24 {
+		hour %= 24
+	}
+
+	if hour < 0 {
+		hour = 24 + (hour % 24)
+	}
 	return Clock{
 		hour:   hour,
 		minute: minute,
@@ -13,19 +33,31 @@ func New(hour, minute int) Clock {
 }
 
 func (clock Clock) String() string {
-	return ""
+	return fmt.Sprintf("%02d:%02d", clock.hour, clock.minute)
 }
 
 func (clock Clock) Add(minutes int) Clock {
-	return Clock{
-		hour:   clock.hour,
-		minute: clock.minute,
+	updatedClock := New(clock.hour, clock.minute)
+	updatedClock.minute += minutes
+	if updatedClock.minute > 60 {
+		updatedClock.hour += clock.minute / 60
+		updatedClock.minute = clock.minute % 60
 	}
+	if updatedClock.hour > 24 {
+		updatedClock.hour = clock.hour % 24
+	}
+	return updatedClock
 }
 
-func (clock Clock) Substract(minutes int) Clock {
-	return Clock{
-		hour:   clock.hour,
-		minute: clock.minute,
+func (clock Clock) Subtract(minutes int) Clock {
+	updatedClock := New(clock.hour, clock.minute)
+	updatedClock.minute -= minutes
+	if updatedClock.minute < 0 {
+		updatedClock.hour += (clock.minute - 60) / 60
+		updatedClock.minute = 60 + clock.minute%60
 	}
+	if updatedClock.hour < 0 {
+		updatedClock.hour = 24 + (clock.hour % 24)
+	}
+	return updatedClock
 }
