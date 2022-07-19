29a7980de0113c84d9c0559ca202f6ffa1d9768c
Jonathan Lopez <jonathanglopez@gmail.com>
2022-07-19 15:58:46 -0400

    Passed all tests

---
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,22 @@
+export const colorValues = new Map<string, number>([
+  ['black', 0,],
+  ['brown', 1],
+  ['red', 2],
+  ['orange', 3],
+  ['yellow', 4],
+  ['green', 5],
+  ['blue', 6],
+  ['violet', 7],
+  ['grey', 8],
+  ['white', 9],
+])
+
+export function decodedValue(colors: string[]): number {
+  let result: number = 0
+  let position: number = 10
+  for (let color of colors.slice(0, 2)) {
+    result += position * colorValues.get(color)!
+    position /= 10;
+  }
+  return result
+}
