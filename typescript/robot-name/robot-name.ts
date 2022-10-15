141fbe1900cd7458f5f3ae2e0981866b707b4018
Jonathan Lopez <jonathanglopez@gmail.com>
2022-10-15 11:25:51 -0400

    Clean up implementation

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,6 +1,6 @@
 class NameGenerator {
-  unusedNames: Array<number>;
-  letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  static letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  private unusedNames: Array<number>;
 
   constructor() {
     const uniqueNameCount = 26 * 26 * 10 * 10 * 10;
@@ -12,15 +12,15 @@ class NameGenerator {
 
   public nextName(): string {
     const nextUnusedIndex = Math.floor(Math.random() * this.unusedNames.length);
-    const nextIndex = this.unusedNames.splice(nextUnusedIndex, 1)[0];
-    return this.indexToName(nextIndex);
+    const nextNameIndex = this.unusedNames.splice(nextUnusedIndex, 1)[0];
+    return this.indexToName(nextNameIndex);
   }
 
   private indexToName(index: number): string {
     let name = String(index % 1000).padStart(3, "0");
     index = Math.floor((index - (index % 1000)) / 1000);
     for (let i = 0; i < 2; i++) {
-      name = this.letters[index % 26] + name;
+      name = NameGenerator.letters[index % 26] + name;
       index = Math.floor((index - (index % 26)) / 26);
     }
     return name;
