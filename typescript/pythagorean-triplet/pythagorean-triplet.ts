72b84f31fbae497bf08adaaeddb81ab5e834819b
Jonathan Lopez <jonathanglopez@gmail.com>
2022-10-17 17:04:47 -0400

    Passed all tests

--- typescript/pythagorean-triplet/pythagorean-triplet.ts
+++ typescript/pythagorean-triplet/pythagorean-triplet.ts
@@ -1,19 +1,35 @@
 type Options = {
-  minFactor?: number
-  maxFactor?: number
-  sum: number
-}
+  minFactor?: number;
+  maxFactor?: number;
+  sum: number;
+};
 
-export function triplets({}: Options): Triplet[] {
-  throw new Error('Remove this statement and implement this function')
+export function triplets({ minFactor, maxFactor, sum }: Options): Triplet[] {
+  const result: Array<Triplet> = [];
+  maxFactor = maxFactor ?? sum;
+  for (let a = minFactor ?? 1; a < sum; a++) {
+    for (let b = a + 1; b < sum; b++) {
+      const c = sum - (a + b);
+      if (c < maxFactor && a * a + b * b === c * c) {
+        result.push(new Triplet(a, b, c));
+      }
+    }
+    2;
+  }
+  return result;
 }
 
 class Triplet {
-  constructor() {
-    throw new Error('Remove this statement and implement this function')
+  a: number;
+  b: number;
+  c: number;
+  constructor(a: number, b: number, c: number) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
   }
 
   toArray(): [number, number, number] {
-    throw new Error('Remove this statement and implement this function')
+    return [this.a, this.b, this.c];
   }
 }
--- typescript/pythagorean-triplet/pythagorean-triplet_test.ts
+++ typescript/pythagorean-triplet/pythagorean-triplet_test.ts
@@ -15,26 +15,26 @@ describe("Triplet", () => {
     assertEquals(tripletsWithSum(12), [[3, 4, 5]]);
   });
 
-  it.ignore("triplets whose sum is 108", () => {
+  it("triplets whose sum is 108", () => {
     assertEquals(tripletsWithSum(108), [[27, 36, 45]]);
   });
 
-  it.ignore("triplets whose sum is 1000", () => {
+  it("triplets whose sum is 1000", () => {
     assertEquals(tripletsWithSum(1000), [[200, 375, 425]]);
   });
 
-  it.ignore("no matching triplets for 1001", () => {
+  it("no matching triplets for 1001", () => {
     assertEquals(tripletsWithSum(1001), []);
   });
 
-  it.ignore("returns all matching triplets", () => {
+  it("returns all matching triplets", () => {
     assertEquals(tripletsWithSum(90), [
       [9, 40, 41],
       [15, 36, 39],
     ]);
   });
 
-  it.ignore("several matching triplets", () => {
+  it("several matching triplets", () => {
     assertEquals(tripletsWithSum(840), [
       [40, 399, 401],
       [56, 390, 394],
@@ -47,22 +47,22 @@ describe("Triplet", () => {
     ]);
   });
 
-  it.ignore("returns triplets with no factor smaller than minimum factor", () => {
+  it("returns triplets with no factor smaller than minimum factor", () => {
     assertEquals(tripletsWithSum(90, { minFactor: 10 }), [[15, 36, 39]]);
   });
 
-  it.ignore("returns triplets with no factor larger than maximum factor", () => {
+  it("returns triplets with no factor larger than maximum factor", () => {
     assertEquals(tripletsWithSum(840, { maxFactor: 349 }), [[240, 252, 348]]);
   });
 
-  it.ignore("returns triplets with factors in range", () => {
+  it("returns triplets with factors in range", () => {
     assertEquals(tripletsWithSum(840, { maxFactor: 352, minFactor: 150 }), [
       [210, 280, 350],
       [240, 252, 348],
     ]);
   });
 
-  it.ignore("triplets for large number", () => {
+  it("triplets for large number", () => {
     assertEquals(tripletsWithSum(30000), [
       [1200, 14375, 14425],
       [1875, 14000, 14125],
