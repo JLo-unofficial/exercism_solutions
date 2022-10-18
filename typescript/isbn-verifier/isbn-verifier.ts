89a70b4204e6627a97ac23bbc2939afb69bdf6bf
Jonathan Lopez <jonathanglopez@gmail.com>
2022-10-18 13:31:36 -0400

    Passed all tests

--- typescript/isbn-verifier/isbn-verifier.ts
+++ typescript/isbn-verifier/isbn-verifier.ts
@@ -1,3 +1,15 @@
 export function isValid(isbn: string): boolean {
-  return true;
+  const isbnPattern = /^\d\-?\d{3}\-?\d{5}\-?[\dX]$/;
+  if (!isbn.match(isbnPattern)) {
+    return false;
+  }
+  return isbn.split("")
+        .filter((char) => char !== "-")
+        .map((char, index) =>
+          (10 - index) * (char === "X" ? 10 : parseInt(char))
+        )
+        .reduce(
+          (runningTotal, currentValue) => (currentValue + runningTotal),
+          0,
+        ) % 11 === 0;
 }
--- typescript/isbn-verifier/isbn-verifier_test.ts
+++ typescript/isbn-verifier/isbn-verifier_test.ts
@@ -10,55 +10,55 @@ describe("ISBN Verifier", () => {
     assert(isValid("3-598-21508-8"));
   });
 
-  it.ignore("invalid isbn check digit", () => {
+  it("invalid isbn check digit", () => {
     assertFalse(isValid("3-598-21508-9"));
   });
 
-  it.ignore("valid isbn number with a check digit of 10", () => {
+  it("valid isbn number with a check digit of 10", () => {
     assert(isValid("3-598-21507-X"));
   });
 
-  it.ignore("check digit is a character other than X", () => {
+  it("check digit is a character other than X", () => {
     assertFalse(isValid("3-598-21507-A"));
   });
 
-  it.ignore("invalid character in isbn", () => {
+  it("invalid character in isbn", () => {
     assertFalse(isValid("3-598-2K507-0"));
   });
 
-  it.ignore("X is only valid as a check digit", () => {
+  it("X is only valid as a check digit", () => {
     assertFalse(isValid("3-598-2X507-9"));
   });
 
-  it.ignore("valid isbn without separating dashes", () => {
+  it("valid isbn without separating dashes", () => {
     assert(isValid("3598215088"));
   });
 
-  it.ignore("isbn without separating dashes and X as check digit", () => {
+  it("isbn without separating dashes and X as check digit", () => {
     assert(isValid("359821507X"));
   });
 
-  it.ignore("isbn without check digit and dashes", () => {
+  it("isbn without check digit and dashes", () => {
     assertFalse(isValid("359821507"));
   });
 
-  it.ignore("too long isbn and no dashes", () => {
+  it("too long isbn and no dashes", () => {
     assertFalse(isValid("3598215078X"));
   });
 
-  it.ignore("isbn without check digit", () => {
+  it("isbn without check digit", () => {
     assertFalse(isValid("3-598-21507"));
   });
 
-  it.ignore("too long isbn", () => {
+  it("too long isbn", () => {
     assertFalse(isValid("3-598-21507-XX"));
   });
 
-  it.ignore("check digit of X should not be used for 0", () => {
+  it("check digit of X should not be used for 0", () => {
     assertFalse(isValid("3-598-21515-X"));
   });
 
-  it.ignore("empty isbn", () => {
+  it("empty isbn", () => {
     assertFalse(isValid(""));
   });
 });
