504dbb1e412173be26127ab5b8d4be53f0384f99
Jonathan Lopez <jonathanglopez@gmail.com>
2022-10-18 21:06:08 -0400

    Convert expect and xit lines to deno test lines

--- scripts/nodeToDeno/main.ts
+++ scripts/nodeToDeno/main.ts
@@ -1,5 +1,4 @@
 import { expandGlob } from "https://deno.land/std@0.160.0/fs/expand_glob.ts";
-import { move } from "https://deno.land/std@0.160.0/fs/mod.ts";
 
 async function generateImportMap() {
   const latestVersion = "0.160.0";
@@ -41,8 +40,33 @@ async function deleteNodeFiles() {
 }
 
 async function renameTestFile() {
-  for await (const testFile of expandGlob("*.test.ts")) {
-    await move(testFile.name, testFile.name.replace(".test", "_test"));
+  const xit = /^(?<indent>\s*)xit\(/;
+  const toEqual =
+    /^(?<indent>\s*)expect\((?<actual>.+)\)\.toEqual\((?<expected>.*)\)$/;
+  const assertTypes: Set<string> = new Set<string>();
+  for await (const jestFile of expandGlob("*.test.ts")) {
+    const denoTestFile = jestFile.name.replace(".test", "_test");
+    const nodeTestFile = await Deno.readTextFile(jestFile.name);
+    const testLines = nodeTestFile.split("\n");
+    testLines.unshift('import { describe, it } from "testing/bdd.ts";');
+    for (let i = 0; i < testLines.length; i++) {
+      if (xit.test(testLines[i])) {
+        testLines[i] = testLines[i].replace(xit, "$<indent>it.ignore(");
+      } else if (toEqual.test(testLines[i])) {
+        assertTypes.add("assertEquals");
+        testLines[i] = testLines[i].replace(
+          toEqual,
+          "$<indent>assertEquals($<actual>, $<expected>);",
+        );
+      }
+    }
+    testLines.unshift(
+      `import { ${
+        Array.from(assertTypes).join(", ")
+      } } from "testing/asserts.ts";`,
+    );
+    Deno.writeTextFile(denoTestFile, testLines.join("\n"));
+    Deno.remove(jestFile.name);
   }
 }
 
