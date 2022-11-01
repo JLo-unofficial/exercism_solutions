80e6fb03bc740087e20e675d43d311bf388817a5
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-01 19:47:57 -0400

    Passed all tests

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,7 +1,20 @@
+use std::cmp::Ordering;
+
+/// Binary search implementation
+///
+/// * `array`: Search space
+/// * `key`: Target item
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    unimplemented!(
-        "Using the binary search algorithm, find the element '{}' in the array '{:?}' and return its index.",
-        key,
-        array
-    );
+    let mut low: usize = 0;
+    let mut high: usize = array.len();
+
+    while low < high {
+        let middle = (high + low) / 2;
+        match array[middle].cmp(&key) {
+            Ordering::Equal => return Some(middle),
+            Ordering::Greater => high = middle,
+            Ordering::Less => low = middle + 1,
+        }
+    }
+    None
 }
