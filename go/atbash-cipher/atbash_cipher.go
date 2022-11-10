22e45ebfe0dfae671c41f79894109528a63f5d10
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-10 06:03:46 -0500

    First attempt

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,5 +1,30 @@
 package atbash
 
+import "unicode"
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	encoded := encode(&s)
+	return string(encoded)
+}
+
+func encode(s *string) []rune {
+	result := make([]rune, 1, len(*s))
+	for _, char := range *s {
+		if unicode.IsDigit(char) {
+			result = append(result, char)
+		}
+		if !unicode.IsLetter(char) {
+			continue
+		}
+		if unicode.IsUpper(char) {
+			result = append(result, plainToCipher(unicode.ToLower(char)))
+		} else {
+			result = append(result, plainToCipher(char))
+		}
+	}
+	return result
+}
+
+func plainToCipher(char rune) rune {
+	return 'z' - (char - 'a')
 }
