39be4a16d7cfebc1aaf5ba4c22ac7013a2df60d4
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-10 05:13:20 -0500

    Adding tsdocs

--- typescript/atbash-cipher/atbash-cipher.ts
+++ typescript/atbash-cipher/atbash-cipher.ts
@@ -1,49 +1,84 @@
 const firstLetterIdx = "a".charCodeAt(0);
 const lastLetterIdx = "z".charCodeAt(0);
 
-function plainToCipher(letter: string): string {
-  if (/\d/.test(letter)) {
-    return letter;
+/**
+ * Converts a plaintext character into its corresponding Atbash cipher character
+ *
+ * @param plainChar - An alphanumeric string of length 1
+ * @returns Corresponding ciphertext character
+ */
+function plainToCipher(plainChar: string): string {
+  if (/\d/.test(plainChar)) {
+    return plainChar;
   }
   return String.fromCharCode(
-    lastLetterIdx - (letter.toLowerCase().charCodeAt(0) - firstLetterIdx),
+    lastLetterIdx - (plainChar.toLowerCase().charCodeAt(0) - firstLetterIdx),
   );
 }
 
-function cipherToPlain(letter: string): string {
-  if (/\d/.test(letter)) {
-    return letter;
+/**
+ * Converts an Atbash ciphertext character to its corresponding plaintext character
+ *
+ * @param cipherChar - Atbash cipher character
+ * @returns Corresponding plaintext character
+ */
+function cipherToPlain(cipherChar: string): string {
+  if (/\d/.test(cipherChar)) {
+    return cipherChar;
   }
   return String.fromCharCode(
-    firstLetterIdx + (lastLetterIdx - letter.charCodeAt(0)),
+    firstLetterIdx + (lastLetterIdx - cipherChar.charCodeAt(0)),
   );
 }
 
-function createWordsOfLengthFive(words: string[], letter: string): string[] {
-  const lastIdx = words.length - 1;
-  const lastWord = words[lastIdx];
-  if (lastWord.length < 5) {
-    words[lastIdx] += letter;
-  } else {
-    words.push(letter);
-  }
-  return words;
+/**
+ * Higher order function returning a function to group characters
+ * into groups of arbitrary lengths
+ *
+ * @param groupLength - Desired length of Atbash ciphertext groups
+ * @returns Function
+ */
+function createGroupsOfLength(groupLength: number) {
+  return function (words: string[], letter: string): string[] {
+    const lastIdx = words.length - 1;
+    const lastWord = words[lastIdx];
+    if (lastWord.length < groupLength) {
+      words[lastIdx] += letter;
+    } else {
+      words.push(letter);
+    }
+    return words;
+  };
 }
 
+/**
+ * Simple regex arrow function to check if a character is a digit or a letter (case-insensitive)
+ *
+ * @param char - Single character string
+ * @returns true if the character is a digit or letter (case-insensitive)
+ */
 const isAlphaNumeric = (char: string): boolean => /[\dA-Za-z]/.test(char);
 
 /**
+ * Encode a plaintext message using the Atbash cipher
+ *
  * @param plainText - Message to encode
- * @returns Message encrypted using atbash cipher
+ * @returns Encoded message using Atbash cipher
  */
 export function encode(plainText: string): string {
   return [...plainText]
     .filter(isAlphaNumeric)
     .map(plainToCipher)
-    .reduce(createWordsOfLengthFive, [""])
+    .reduce(createGroupsOfLength(5), [""])
     .join(" ");
 }
 
+/**
+ * Decode an Atbash encoded ciphertext message to plaintext
+ *
+ * @param cipherText - Encoded message using the Atbash cipher
+ * @returns Plaintext message without any spacing
+ */
 export function decode(cipherText: string): string {
   return [...cipherText]
     .filter(isAlphaNumeric)
