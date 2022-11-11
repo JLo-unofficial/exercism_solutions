9a271ee64d7a5345f2388ca5001b8871510ed11f
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-11 08:26:46 -0500

    Passed all tests

--- typescript/phone-number/phone-number.ts
+++ typescript/phone-number/phone-number.ts
@@ -1,5 +1,36 @@
+const isValidAreaCode = (phoneNumber: string) => /^[^01]/.test(phoneNumber);
+const isValidExchangeCode = (phoneNumber: string) =>
+  /^\d{3}[^01]/.test(phoneNumber);
+
 export function clean(messyNumber: string): string {
-  const numbersOnly = [...messyNumber].filter((char: string) => /\d/.test(char))
+  let phoneNumber = [...messyNumber]
+    .filter((char: string) => /\d/.test(char))
     .join("");
-  return numbersOnly;
+
+  if (phoneNumber.length === 11) {
+    if (phoneNumber[0] !== "1") {
+      throw "11 digits must start with 1";
+    }
+    phoneNumber = phoneNumber.substring(1);
+  }
+
+  if (phoneNumber.length !== 10) {
+    throw "Incorrect number of digits";
+  }
+
+  if (!isValidAreaCode(phoneNumber)) {
+    if (phoneNumber[0] === "0") {
+      throw "Area code cannot start with zero";
+    }
+    throw "Area code cannot start with one";
+  }
+
+  if (!isValidExchangeCode(phoneNumber)) {
+    if (phoneNumber[3] === "0") {
+      throw "Area code cannot start with zero";
+    }
+    throw "Area code cannot start with one";
+  }
+
+  return phoneNumber;
 }
--- typescript/phone-number/phone-number_test.ts
+++ typescript/phone-number/phone-number_test.ts
@@ -8,92 +8,92 @@ describe("Phone Number", () => {
       assertEquals(clean("(223) 456-7890"), "2234567890");
     });
 
-    it.ignore("cleans numbers with dots", () => {
+    it("cleans numbers with dots", () => {
       assertEquals(clean("223.456.7890"), "2234567890");
     });
 
-    it.ignore("cleans numbers with multiple spaces", () => {
+    it("cleans numbers with multiple spaces", () => {
       assertEquals(clean("223 456   7890   "), "2234567890");
     });
 
-    it.ignore("invalid when 9 digits", () => {
+    it("invalid when 9 digits", () => {
       assertThrows(() => clean("123456789"), "Incorrect number of digits");
     });
 
-    it.ignore("invalid when 11 digits does not start with a 1", () => {
+    it("invalid when 11 digits does not start with a 1", () => {
       assertThrows(() => clean("22234567890"), "11 digits must start with 1");
     });
 
-    it.ignore("valid when 11 digits and starting with 1", () => {
+    it("valid when 11 digits and starting with 1", () => {
       assertEquals(clean("12234567890"), "2234567890");
     });
 
-    it.ignore("valid when 11 digits and starting with 1 even with punctuation", () => {
+    it("valid when 11 digits and starting with 1 even with punctuation", () => {
       assertEquals(clean("+1 (223) 456-7890"), "2234567890");
     });
 
-    it.ignore("invalid when more than 11 digits", () => {
+    it("invalid when more than 11 digits", () => {
       assertThrows(() => clean("321234567890"), "More than 11 digits");
     });
 
-    it.ignore("invalid with letters", () => {
+    it("invalid with letters", () => {
       assertThrows(() => clean("123-abc-7890"), "Letters not permitted");
     });
 
-    it.ignore("invalid with punctuations", () => {
+    it("invalid with punctuations", () => {
       assertThrows(() => clean("123-@:!-7890"), "Punctuations not permitted");
     });
 
-    it.ignore("invalid if area code starts with 0", () => {
+    it("invalid if area code starts with 0", () => {
       assertThrows(
         () => clean("(023) 456-7890"),
         "Area code cannot start with zero",
       );
     });
 
-    it.ignore("invalid if area code starts with 1", () => {
+    it("invalid if area code starts with 1", () => {
       assertThrows(
         () => clean("(123) 456-7890"),
         "Area code cannot start with one",
       );
     });
 
-    it.ignore("invalid if exchange code starts with 0", () => {
+    it("invalid if exchange code starts with 0", () => {
       assertThrows(
         () => clean("(223) 056-7890"),
         "Exchange code cannot start with zero",
       );
     });
 
-    it.ignore("invalid if exchange code starts with 1", () => {
+    it("invalid if exchange code starts with 1", () => {
       assertThrows(
         () => clean("(223) 156-7890"),
         "Exchange code cannot start with one",
       );
     });
 
-    it.ignore("invalid if area code starts with 0 on valid 11-digit number", () => {
+    it("invalid if area code starts with 0 on valid 11-digit number", () => {
       assertThrows(
         () => clean("1 (023) 456-7890"),
         "Area code cannot start with zero",
       );
     });
 
-    it.ignore("invalid if area code starts with 1 on valid 11-digit number", () => {
+    it("invalid if area code starts with 1 on valid 11-digit number", () => {
       assertThrows(
         () => clean("1 (123) 456-7890"),
         "Area code cannot start with one",
       );
     });
 
-    it.ignore("invalid if exchange code starts with 0 on valid 11-digit number", () => {
+    it("invalid if exchange code starts with 0 on valid 11-digit number", () => {
       assertThrows(
         () => clean("1 (223) 056-7890"),
         "Exchange code cannot start with zero",
       );
     });
 
-    it.ignore("invalid if exchange code starts with 1 on valid 11-digit number", () => {
+    it("invalid if exchange code starts with 1 on valid 11-digit number", () => {
       assertThrows(
         () => clean("1 (223) 156-7890"),
         "Exchange code cannot start with one",
