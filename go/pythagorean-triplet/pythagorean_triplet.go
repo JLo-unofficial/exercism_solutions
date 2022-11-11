9014485228c7e70eda7646b41aae0362359da59e
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-11 15:50:25 -0500

    Passed all tests with naive implementation

--- go/pythagorean-triplet/pythagorean_triplet.go
+++ go/pythagorean-triplet/pythagorean_triplet.go
@@ -1,1 +1,33 @@
 package pythagorean
+
+type Triplet struct {
+	a, b, c int
+}
+
+func Range(min, max int) []Triplet {
+	result := make([]Triplet, 0)
+	for a := min; a < max-2; a++ {
+		for b := a + 1; b < max; b++ {
+			for c := b + 1; c <= max; c++ {
+				if a*a+b*b == c*c {
+					result = append(result, Triplet{a: a, b: b, c: c})
+				}
+			}
+		}
+	}
+	return result
+}
+
+func Sum(sum int) []Triplet {
+	var c int
+	result := make([]Triplet, 0)
+	for a := 1; a < sum-2; a++ {
+		for b := a + 1; b < sum; b++ {
+			c = sum - (a + b)
+			if a*a+b*b == c*c {
+				result = append(result, Triplet{a: a, b: b, c: c})
+			}
+		}
+	}
+	return result
+}
