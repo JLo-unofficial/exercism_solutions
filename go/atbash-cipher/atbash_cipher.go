48b3c913b94141d187fdc0ca2ad04838224c448d
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-11 12:21:52 -0500

    Adding comments

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -12,29 +12,39 @@ var (
 )
 
 func Atbash(s string) string {
-	var message strings.Builder
+	var encodedMessage strings.Builder
 	var cipherText strings.Builder
 	for i := 0; i < len(s); i++ {
+		// Digits can be directly written to cipherText
 		if s[i] >= zero && s[i] <= nine {
 			cipherText.WriteByte(s[i])
 		}
+
+		// Convert uppercase letters to lowercase
 		if s[i] >= A && s[i] <= Z {
 			cipherText.WriteByte(plainToCipher(s[i] - (A - a)))
 		}
+
 		if s[i] >= a && s[i] <= z {
 			cipherText.WriteByte(plainToCipher(s[i]))
 		}
 
+		// cipherText should only hold strings up to len 5
 		if cipherText.Len() == 5 {
-			message.WriteString(cipherText.String())
-			message.WriteString(" ")
+			encodedMessage.WriteString(cipherText.String())
+			encodedMessage.WriteString(" ")
 			cipherText.Reset()
 		}
 	}
-	message.WriteString(cipherText.String())
-	return strings.Trim(message.String(), " ")
+
+	// Write cipherText of len < 4
+	encodedMessage.WriteString(cipherText.String())
+
+	// Trim trailing space from line 35
+	return strings.Trim(encodedMessage.String(), " ")
 }
 
+// Helper function to encode plainText character to its corresponding cipherText
 func plainToCipher(char byte) byte {
 	return z - (char - a)
 }
