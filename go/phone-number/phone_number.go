f7915fd282696795bdace0bd2cb83258c3c64d66
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-11 14:48:42 -0500

    Cleaned up and refactored code

--- go/phone-number/phone_number.go
+++ go/phone-number/phone_number.go
@@ -4,43 +4,38 @@ import (
 	"errors"
 	"fmt"
 	"strings"
-)
-
-var (
-	zero byte = "0"[0]
-	one  byte = "1"[0]
-	nine byte = "9"[0]
+	"unicode"
 )
 
 func Number(phoneNumber string) (string, error) {
-	var numbersOnly strings.Builder
-	for i := 0; i < len(phoneNumber); i++ {
-		if phoneNumber[i] >= zero && phoneNumber[i] <= nine {
-			numbersOnly.WriteByte(phoneNumber[i])
+	var digits strings.Builder
+	for _, digit := range phoneNumber {
+		if unicode.IsDigit(digit) {
+			digits.WriteRune(digit)
 		}
 	}
-	cleanedNumber := numbersOnly.String()
+	phoneNumber = digits.String()
 
 	// Remove leading country code if present
-	if len(cleanedNumber) == 11 && strings.HasPrefix(cleanedNumber, "1") {
-		cleanedNumber = cleanedNumber[1:]
+	if len(phoneNumber) == 11 && phoneNumber[0] == '1' {
+		phoneNumber = phoneNumber[1:]
 	}
 
 	// Only numbers of length 10 are valid
-	if len(cleanedNumber) != 10 {
+	if len(phoneNumber) != 10 {
 		return "", errors.New("invalid number of digits")
 	}
 
 	// Invalidate numbers with a 0 or 1 in area code
-	if cleanedNumber[0] == zero || cleanedNumber[0] == one {
+	if phoneNumber[0] == '0' || phoneNumber[0] == '1' {
 		return "", errors.New("area code cannot start with zero or one")
 	}
 	// Invalidate numbers with a 0 or 1 in exchange code
-	if cleanedNumber[3] == zero || cleanedNumber[3] == one {
+	if phoneNumber[3] == '0' || phoneNumber[3] == '1' {
 		return "", errors.New("exchange code cannot start with zero or one")
 	}
 
-	return cleanedNumber, nil
+	return phoneNumber, nil
 }
 
 func AreaCode(phoneNumber string) (string, error) {
