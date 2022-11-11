97f28226bbd8738a23ef49c84c8f1d4008ee6994
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-11 16:00:12 -0500

    Slightly improve performace

--- go/pythagorean-triplet/pythagorean_triplet.go
+++ go/pythagorean-triplet/pythagorean_triplet.go
@@ -21,11 +21,12 @@ func Range(min, max int) []Triplet {
 func Sum(sum int) []Triplet {
 	var c int
 	result := make([]Triplet, 0)
-	for a := 1; a < sum-2; a++ {
-		for b := a + 1; b < sum; b++ {
+	for a := 1; a <= sum/3; a++ {
+		for b := a + 1; b < (sum-a)/2; b++ {
 			c = sum - (a + b)
 			if a*a+b*b == c*c {
 				result = append(result, Triplet{a: a, b: b, c: c})
+				break
 			}
 		}
 	}
