475efdefb2a82fa591cdd2bff65c23be8d6c0d7b
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-11 07:34:52 -0500

    Passed first test for real

--- typescript/matching-brackets/matching-brackets.ts
+++ typescript/matching-brackets/matching-brackets.ts
@@ -1,3 +1,25 @@
+const isBracket = (char: string): boolean => /[\(\)\{\}\[\]]/.test(char);
+
+const complement: Map<string, string> = new Map<string, string>([
+  ["[", "]"],
+]);
+
 export function isPaired(input: string): boolean {
-  return true;
+  const stack: Array<string> = [];
+  for (const char of input) {
+    if (!isBracket(char)) {
+      continue;
+    }
+
+    stack.push(char);
+    if (stack.length < 2) {
+      continue;
+    }
+
+    if (stack[stack.length - 1] === complement.get(stack[stack.length - 2])) {
+      stack.pop();
+      stack.pop();
+    }
+  }
+  return stack.length === 0;
 }
