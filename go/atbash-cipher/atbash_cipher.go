80938dc72486ea9b71fc1b321081a26092f0257e
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-12 21:47:08 -0500

    clean up implementation

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -6,38 +6,29 @@ import (
 )
 
 func Atbash(s string) string {
-	var encodedMessage strings.Builder
 	var cipherText strings.Builder
 	for _, char := range s {
+		// cipherText should only hold strings up to len 5
+		if cipherText.Len()%6 == 5 {
+			cipherText.WriteRune(' ')
+		}
 		switch {
 
 		// Digits can be directly written to cipherText
 		case unicode.IsDigit(char):
 			cipherText.WriteRune(char)
 
-		case unicode.IsUpper(char):
-			cipherText.WriteRune(plainToCipher(unicode.ToLower(char)))
-
-		case unicode.IsLower(char):
+		case unicode.IsLetter(char):
 			cipherText.WriteRune(plainToCipher(char))
 		}
 
-		// cipherText should only hold strings up to len 5
-		if cipherText.Len() == 5 {
-			encodedMessage.WriteString(cipherText.String())
-			encodedMessage.WriteString(" ")
-			cipherText.Reset()
-		}
 	}
 
-	// Write cipherText of len < 4
-	encodedMessage.WriteString(cipherText.String())
-
 	// Trim trailing space from line 35
-	return strings.Trim(encodedMessage.String(), " ")
+	return strings.Trim(cipherText.String(), " ")
 }
 
 // Helper function to encode plainText character to its corresponding cipherText
 func plainToCipher(char rune) rune {
-	return 'z' - (char - 'a')
+	return 'z' - (unicode.ToLower(char) - 'a')
 }
