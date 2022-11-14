486f3f51887549004eb16d1c94c3d1486e226eb4
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-14 11:40:20 -0500

    Optimized result

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,15 +1,17 @@
 package acronym
 
 import (
-	"regexp"
 	"strings"
 	"unicode"
 )
 
+func abbrevSplit(char rune) bool {
+	return char == ' ' || char == '_' || char == '-'
+}
+
 func Abbreviate(s string) string {
 	result := strings.Builder{}
-	wordSplitPattern := regexp.MustCompile(`[\_\-\s]+`)
-	for _, word := range wordSplitPattern.Split(s, -1) {
+	for _, word := range strings.FieldsFunc(s, abbrevSplit) {
 		result.WriteRune(unicode.ToUpper(rune(word[0])))
 	}
 	return result.String()
