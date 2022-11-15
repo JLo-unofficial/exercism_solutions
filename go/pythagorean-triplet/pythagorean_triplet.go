69bc6d97ca86eed85e6a58c926fac0ff3e2a452a
Jonathan Lopez <jonathanglopez@gmail.com>
2022-11-15 12:29:34 -0500

    Refactor to use named returns

--- go/pythagorean-triplet/pythagorean_triplet.go
+++ go/pythagorean-triplet/pythagorean_triplet.go
@@ -4,18 +4,18 @@ type Triplet struct {
 	a, b, c int
 }
 
-func Range(min, max int) []Triplet {
-	result := make([]Triplet, 0)
+func Range(min, max int) (triplets []Triplet) {
+	triplets = make([]Triplet, 0)
 	for a := min; a < max-2; a++ {
 		for b := a + 1; b < max; b++ {
 			for c := b + 1; c <= max; c++ {
 				if a*a+b*b == c*c {
-					result = append(result, Triplet{a: a, b: b, c: c})
+					triplets = append(triplets, Triplet{a: a, b: b, c: c})
 				}
 			}
 		}
 	}
-	return result
+	return triplets
 }
 
 func Sum(sum int) []Triplet {
