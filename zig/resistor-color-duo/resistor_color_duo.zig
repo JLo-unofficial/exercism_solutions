2390cd170520474d764b5e960ef1b64368118229
Jonathan Lopez <jonathanglopez@gmail.com>
2023-02-24 11:03:14 -0500

    Passed all tests

--- zig/resistor-color-duo/resistor_color_duo.zig
+++ zig/resistor-color-duo/resistor_color_duo.zig
@@ -1,3 +1,23 @@
+const std = @import("std");
+pub const ColorBand = enum {
+    black,
+    brown,
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    violet,
+    grey,
+    white,
+};
+
 pub fn colorCode(colors: []const ColorBand) anyerror!isize {
-    @panic("please implement the color_code function");
+    comptime {
+        // tens position
+        const a: isize = @intCast(isize, 10) * @enumToInt(colors[0]);
+        // ones position
+        const b: isize = @enumToInt(colors[1]);
+        return a + b;
+    }
 }
