23cd3871fb200544df2dbe1d59cb2c141137c13d
Jonathan Lopez <jonathanglopez@gmail.com>
2023-02-28 10:00:46 -0500

    Refactor answer

--- zig/darts/darts.zig
+++ zig/darts/darts.zig
@@ -5,13 +5,21 @@ pub const Coordinate = struct {
     pub fn init(x_coord: f32, y_coord: f32) Coordinate {
         return Coordinate{ .x = x_coord, .y = y_coord };
     }
+
     pub fn score(self: Coordinate) isize {
-        const hypotenuse = @sqrt(self.x * self.x + self.y * self.y);
-        if (hypotenuse <= 1) {
+        // Avoid using the expensive @sqrt function by squaring all values
+        const distance = self.x * self.x + self.y * self.y;
+
+        // Dart board circles' radii squared
+        const inner = 1 * 1;
+        const middle = 5 * 5;
+        const outer = 10 * 10;
+
+        if (distance <= inner) {
             return 10;
-        } else if (hypotenuse <= 5) {
+        } else if (distance <= middle) {
             return 5;
-        } else if (hypotenuse <= 10) {
+        } else if (distance <= outer) {
             return 1;
         } else {
             return 0;
